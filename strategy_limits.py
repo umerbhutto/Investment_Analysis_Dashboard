a9eb8b4e136d2d7bfe7efb6810de2dfcaeb8d83f
Umer Bhutto <51999864+umerbhutto@users.noreply.github.com>
2022-07-31 15:04:21 +0500

    "Added strategy limits file"

---
+++ strategy_limits.py
@@ -0,0 +1,14 @@
+# Maximum shareholding - 25%
+max_stock_share = 0.25
+
+# Maximum stock volatility - 40%
+max_stock_volatility = 0.4
+
+# Maximum portfolio volatility - 30%
+max_portfolio_volatility = 0.3
+
+# Target_annualized return
+target_annual_return = 0.15
+
+
+
